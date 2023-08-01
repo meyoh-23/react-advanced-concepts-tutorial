3e6db0ae79efe553bdf21c9a0ad39736a88c5aec
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-01 15:02:07 +0300

    ADVANCED REACT TUORIAL

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Css styles
 import './index.css';
-// named import
-import {books} from './books';
-//default import
-import Book from './Book'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// working with lists
-
-function BookList() {
-  return (
-    <section className="booklist "> { books.map((book, index) => {
-      const {img, title, author} = book
-      return (
-        // passing the key to eliminate the error that is generated without it.
-        <Book key ={book.id} {...book}></Book>
-      )
-    })
-    }
-    </section>
-  )
-};
-
-// actual description of the book
-
 root.render(
-  <BookList/>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
