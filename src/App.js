220a6d80f9fa0bb9da8c9afd93d879e93b41f6f8
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-02 15:42:09 +0300

    useEffect Basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/6-useState-functional-counter';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,28 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('call useEffect');
+    // to implement useEffect only when a certain condition is met.
+    if (value >= 1) {
+      document.title = `New Message(${value})`;
+      }
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Hello from useEffect 🎖️🎖️🎖️🎖️🎖️')
+  },[])
+
+  console.log( 'render component');
+
+  return <>
+    <h1>{value}</h1>
+    <button type='button' className = 'btn' onClick = {() => setValue(value + 1)} >
+      click me
+    </button>
+  </>
 };
 
 export default UseEffectBasics;
