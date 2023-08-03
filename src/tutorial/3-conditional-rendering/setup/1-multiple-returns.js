641daab3e896246eeb455e404238e89936504a39
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-03 08:53:17 +0300

    intro to multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <h2> Loading...</h2>
+  }
   return <h2>multiple returns</h2>;
 };
 
 export default MultipleReturns;
+
+
+// in the above code, we want to have diffrent return based on some conditions
+// if loading is true, we want to have a return, and if loading is false we want to have another return.
