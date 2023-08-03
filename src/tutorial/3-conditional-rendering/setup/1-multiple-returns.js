4e869198aa7398705a57066bd21acadecef6cf71
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-03 10:19:01 +0300

    advancing into multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -2,12 +2,56 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(true);
+  const [user, setUser] = useState('default user');
 
-  if (loading) {
-    return <h2> Loading...</h2>
+// seting up the useEffect
+useEffect(()=>{
+  // fetching data asynchronously
+    fetch(url)
+    .then((resp)=>{
+      if (resp.status >= 200 && resp.status <= 299) {
+        // using the status of the response to handle the error
+        return resp.json()
+      } else {
+        // when error is encountered, stop loading and set error to true
+        // also throw a new error in such a case.
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText)      }
+    }
+      ) // we've successfully fetched the user data
+    .then((user) => {
+      // destructure the user object to get the data we need.
+      const {login} = user;
+      setUser(login);
+      setIsLoading(false);
+    }
+      )
+    .catch(error => console.log(error));
+}, []);
+
+// conditions to be checked for
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
   }
-  return <h2>multiple returns</h2>;
+
+  if (isError) {
+    <div>
+        <h1>Error...</h1>
+      </div>
+  }
+
+  return (
+    <div>
+        <h1>{user}</h1>
+      </div>
+    )
 };
 
 export default MultipleReturns;
