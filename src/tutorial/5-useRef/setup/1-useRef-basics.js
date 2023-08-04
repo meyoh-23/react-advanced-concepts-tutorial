d0cb5c80ce70d81122806e18c75df40c54632e0d
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-04 14:28:31 +0300

    useRef intoduction

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,34 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  // initializing the useRef
+  const refContaner = useRef(null);
+  const divContaner = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContaner.current.value);
+    console.log(divContaner.current);
+  }
+
+  // useEffect to focus pointer to the useRefContainer
+  useEffect(() => {
+    console.log(refContaner.current);
+    refContaner.current.focus();
+  });
+
+  return (
+  <>
+    <form className='form' onSubmit={handleSubmit}>
+      <div>
+        <input type='text' ref={refContaner}></input>
+        <button type='submit'>submit</button>
+      </div>
+      <div ref={divContaner}>hello World from the Div container</div>
+    </form>
+    
+  </>
+  );
 };
 
 export default UseRefBasics;
