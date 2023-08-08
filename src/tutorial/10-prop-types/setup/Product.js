8656746201ac32b9819323eb0a2284033525f474
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-08 13:49:05 +0300

    prop-types tutorial

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from './../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+  //short-circuit alternative
+  const url = image && image.url;
+  return (
+    <article className='product'>
+      <h4>single product</h4>
+      <img src={url || defaultImage} alt={name}/>
+      <h4>{name || 'default name'}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+
+// setting up proptype property on the component
+Product.prototype = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+// setting up the default props
+// default image is not shown because it has been declared as an object and si required in the Product.pototype array shown above
+/* Product.defaultProps = {
+  name: 'default name',
+  price: 3.99, 
+  image: defaultImage 
+} */
 
 export default Product;
