532eaee3ee2930bd538ef247805ab43853884b2f
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-08 08:38:54 +0300

    useContext tutorial

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,9 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,14 +14,16 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+
+    <PersonContext.Provider value={{removePerson, people}}>
+      <h3>Contex API / 'useContext' hook</h3>
+      <List/>
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const { people }= useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
@@ -26,7 +31,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +38,9 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name}) => {
+  const {removePerson }= useContext(PersonContext);
+  //console.log(data)
   return (
     <div className='item'>
       <h4>{name}</h4>
